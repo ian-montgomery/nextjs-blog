d0f14c941437aaae2e64270dd4d0a81028258e61
Ian Montgomery <ianmontgomery@posteo.net>
2021-01-19 18:52:05 +1300

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Ian. I'm a full stack developer. You cant contact me on <a href="https://twitter.com/cool_aubergine">Twitter</a>.</p>
+        <p>Hello, I'm Ian. I'm a full stack developer. You can contact me on <a href="https://twitter.com/cool_aubergine">Twitter</a>.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
